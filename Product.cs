9a180b9f5fd4cc468d1417daaf78661dd115dbbc
Abdul Alsafri <abdulalsafri1@gmail.com>
2025-03-08 18:07:12 -0600

    Product store

---
+++ Product.cs
@@ -0,0 +1,8 @@
+public class Product()
+{
+    public string Name {get; set;}
+    public decimal Price {get; set;}
+    public bool Sold {get; set;}
+    public int ProductTypeId {get; set;}
+    public DateTime DateStocked {get; set;}
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ while (choice != "0")
 {
 Console.WriteLine(@"
 Main Menu:
+0. To exit the program
 1. View all products
 2. Add a product
 3. Delete a product
@@ -13,6 +14,7 @@ Main Menu:
 5. 
 ");
 
+    choice = Console.ReadLine();
     if (choice == "0")
     {
         Console.WriteLine("Are you sure you want to exit the program? (y/n)");
@@ -26,28 +28,34 @@ Main Menu:
         else
         {
             Console.WriteLine("Returning to main menu!");
-            choice = null;
         }
 
     }
     else if (choice == "1")
     {
         Console.WriteLine("Viewing all products..");
-        choice = null;
     }
     else if (choice == "2")
     {
         Console.WriteLine("Adding a products..");
-        choice = null;
     }
     else if (choice == "3")
     {
         Console.WriteLine("Deleting a products..");
-        choice = null;
     }
     else if (choice == "4")
     {
         Console.WriteLine("Updating a products..");
-        choice = null;
     }
+    else if (choice == "5")
+    {
+        Console.WriteLine("Searching for products by type...");
+    }
+    else
+    {
+        Console.WriteLine("Invalid choice. Please enter a valid option.");
+    }
+    Console.WriteLine("\nPress any key to return to the main menu...");
+    Console.ReadKey();
+    Console.Clear();
 }
